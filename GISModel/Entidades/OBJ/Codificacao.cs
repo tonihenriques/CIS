397a853beb97aad494ad7b5269ad2c931268de43
Johnnathan Almeida <johnnathanalmeida22@gmail.com>
2019-05-22 21:27:39 -0300

    Ajustes levantados na última reunião: 35%.

---
+++ GISModel/Entidades/OBJ/Codificacao.cs
@@ -0,0 +1,65 @@
+﻿using GISModel.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GISModel.Entidades.OBJ
+{
+    [Table("OBJCodificacao")]
+    public class Codificacao : EntidadeBase
+    {
+
+        [Display(Name = "Tipo Acidente")]
+        public ETipoAcidente TipoAcidente { get; set; }
+
+        [Display(Name = "Atividade")]
+        public EAtividades Atividade { get; set; }
+
+        [Display(Name = "Tipo Atividade")]
+        public string UKTipoAtividade { get; set; }
+
+        [Display(Name = "Natureza")]
+        public string UKNatureza { get; set; }
+
+        [Display(Name = "Consequência Lesão")]
+        public EConsequencia ConsequenciaLesao { get; set; }
+
+        [Display(Name = "Função GRIDS")]
+        public string UKFuncaoGRIDS { get; set; }
+
+        [Display(Name = "Espécie Acid. Impressoal")]
+        public string UKEspecieAcidImpessoal { get; set; }
+
+        [Display(Name = "Tipo Acid. Pessoal")]
+        public string UKTipoAcidPessoal { get; set; }
+
+        [Display(Name = "Agente Acidente")]
+        public string UKAgenteAcidente { get; set; }
+
+        [Display(Name = "Fonte Lesão")]
+        public string UKFonteLesao { get; set; }
+
+        [Display(Name = "Fator Pessoal Inseg")]
+        public string UKFatorPessoalInseg { get; set; }
+
+        [Display(Name = "Ato Inseguro")]
+        public string UKAtoInseguro { get; set; }
+
+        [Display(Name = "Cond. Ambiental Inseg")]
+        public string UKCondAmbientalInseg { get; set; }
+
+        [Display(Name = "Prej. Material")]
+        public string UKPrejMaterial { get; set; }
+
+        public string Custo { get; set; }
+
+        [Display(Name = "Dias Perdidos")]
+        public string DiasPerdidos { get; set; }
+
+        [Display(Name = "Dias Debitados")]
+        public string DiasDebitados { get; set; }
+
+        [Display(Name = "Data do Óbito")]
+        public string DataObito { get; set; }
+
+    }
+}
