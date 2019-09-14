48dd12a462411bac59b0843e7d5aebb551f2161a
Johnnathan Almeida <johnnathanalmeida22@gmail.com>
2019-09-14 19:15:39 -0300

    Preparando Materiais e População.

---
+++ GISModel/Entidades/OBJ/Populacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GISModel.Entidades.OBJ
+{
+    [Table("OBJPopulacao")]
+    public class Populacao : EntidadeBase
+    {
+
+        [Required(ErrorMessage = "Informe o nome da pessoa a ser cadastrada")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Sexo' é obrigatório")]
+        public string Sexo { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "O campo 'Data de Nascimento' é obrigatório")]
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
